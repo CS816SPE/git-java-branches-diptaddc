f6ec7b4ef745511f2f40435d121db139282f1ed2
Dipta Das Chowdhury <dipta.ddc@gmail.com>
2019-01-17 09:43:41 +0530

    Initial Commit Only Add module

---
+++ cal.java
@@ -0,0 +1,36 @@
+import java.io.*;
+public class cal
+{
+	public static void main(String[] args)
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Operation:\n1.Add\n2.Subtract\n3.Multiply\n4.Divide");
+		int n = Integer.parseInteger(br.readLine());
+		int result = 0;
+		int x = 0;
+		int y = 0;
+		System.out.println("Enter First Number");
+		x = Integer.parseInt(br.readLine());
+		System.out.println("Enter Secong Number");
+		y = Integer.parseInt(br.readLine());
+		switch (n)
+		{
+			case 1:
+			result = x + y;
+			break;
+			case 2:
+			//
+			break;
+			case 3:
+			//
+			break;
+			case 4:
+			//
+			break;
+			default:
+			System.out.println("Invalid Choice");	
+
+		}
+		System.out.println("Answer:" + result);			
+	}
+}
